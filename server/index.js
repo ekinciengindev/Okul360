488ddc7f6afd3cf14bd99414ce3749216d6656ad
ekinciengindev <167542230+ekinciengindev@users.noreply.github.com>
2026-09-13 16:29:48 +0300

    Update CORS allowedHeaders to support Bypass-Tunnel-Reminder

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-school-id']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-school-id', 'Bypass-Tunnel-Reminder']
 }));
 app.use(express.json());
 
